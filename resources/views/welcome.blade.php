037fbe6a32ed838dcbca2cf259ff619ef600c09b
thelf <info@helfrich-media.de>
2021-02-16 15:19:04 +0100

    Content Bereich und Sidebar mit Login für die Desktop Ansicht in 2 Spalten nebeneinander aufgeteilt.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,72 +42,75 @@
                 </form>
             </div>
         </nav>
-        <div class="container-fluid border my-2 py-2">
-            @auth
-            <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
-            @else
-            <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
-
-            @if (Route::has('register'))
-            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-            @endif
-            @endauth
-        </div>
-        <div class="container-fluid">
-            <div class="row border my-2 py-2">
-                <div class="col-3 col-lg-2">
-                    <img src="https://picsum.photos/40" alt="image">
-                </div>
-                <div class="col-6 col-lg-4">
-                    <h1>Section name</h1>
-                </div>
-                <div class="col-3 col-lg-2">7</div>
-                <div class="col-12 col-lg-4">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. </p>
-                </div>
-            </div>
-            <div class="row border my-2 py-2">
-                <div class="col-3 col-lg-2">
-                    <img src="https://picsum.photos/40" alt="image">
-                </div>
-                <div class="col-6 col-lg-4">
-                    <h1>Section name</h1>
-                </div>
-                <div class="col-3 col-lg-2">7</div>
-                <div class="col-12 col-lg-4">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. </p>
-                </div>
-            </div>
-            <div class="row border my-2 py-2">
-                <div class="col-3 col-lg-2">
-                    <img src="https://picsum.photos/40" alt="image">
-                </div>
-                <div class="col-6 col-lg-4">
-                    <h1>Section name</h1>
-                </div>
-                <div class="col-3 col-lg-2">7</div>
-                <div class="col-12 col-lg-4">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. </p>
-                </div>
-            </div>
-            <div class="row border my-2 py-2">
-                <div class="col-3 col-lg-2">
-                    <img src="https://picsum.photos/40" alt="image">
-                </div>
-                <div class="col-6 col-lg-4">
-                    <h1>Section name</h1>
-                </div>
-                <div class="col-3 col-lg-2">7</div>
-                <div class="col-12 col-lg-4">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. </p>
-                </div>
-            </div>
-        </div>
     </div>
+    <div class="container-fluid">
+        <div class="row d-flex flex-lg-row-reverse no-gutters">
+            <div class="col-lg-2 border my-2 py-2">
+                @auth
+                <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
+                @else
+                <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
+
+                @if (Route::has('register'))
+                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                @endif
+                @endauth
+            </div>
+            <div class="col-lg-10">
+                <div class="row border my-2 py-2 pl-2 no-gutters">
+                    <div class="col-3 col-lg-2">
+                        <img src="https://picsum.photos/40" alt="image">
+                    </div>
+                    <div class="col-6 col-lg-4">
+                        <h1>Section name</h1>
+                    </div>
+                    <div class="col-3 col-lg-2">7</div>
+                    <div class="col-12 col-lg-4">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. </p>
+                    </div>
+                </div>
+                <div class="row border my-2 py-2 pl-2 no-gutters">
+                    <div class="col-3 col-lg-2">
+                        <img src="https://picsum.photos/40" alt="image">
+                    </div>
+                    <div class="col-6 col-lg-4">
+                        <h1>Section name</h1>
+                    </div>
+                    <div class="col-3 col-lg-2">7</div>
+                    <div class="col-12 col-lg-4">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. </p>
+                    </div>
+                </div>
+                <div class="row border my-2 py-2 pl-2 no-gutters">
+                    <div class="col-3 col-lg-2">
+                        <img src="https://picsum.photos/40" alt="image">
+                    </div>
+                    <div class="col-6 col-lg-4">
+                        <h1>Section name</h1>
+                    </div>
+                    <div class="col-3 col-lg-2">7</div>
+                    <div class="col-12 col-lg-4">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. </p>
+                    </div>
+                </div>
+                <div class="row border my-2 py-2 pl-2 no-gutters">
+                    <div class="col-3 col-lg-2">
+                        <img src="https://picsum.photos/40" alt="image">
+                    </div>
+                    <div class="col-6 col-lg-4">
+                        <h1>Section name</h1>
+                    </div>
+                    <div class="col-3 col-lg-2">7</div>
+                    <div class="col-12 col-lg-4">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 
