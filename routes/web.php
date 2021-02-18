fb162442aebe847a807f51a0c6d7501b94ac2175
Rhea Marstaller <r.marstaller@gmail.com>
2021-02-18 16:06:58 +0100

    Exchange manual routes with predefined Auth routes.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Foundation\Auth\EmailVerificationRequest;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -17,21 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 /* Routes for authentication and email verification */
-Auth::routes();
-
-Route::get('/email/verify', function () {
-    return view('auth.verify');
-})->middleware('auth')->name('verification.notice');
-
-Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-    $request->fulfill();
-    return redirect('/home');
-})->middleware(['auth', 'signed'])->name('verification.verify');
-
-Route::post('/email/verification-notification', function (Request $request) {
-    $request->user()->sendEmailVerificationNotification();
-    return back()->with('message', 'Verification link sent!');
-})->middleware(['auth', 'throttle:6,1'])->name('verification.resend');
+Auth::routes(['verify' => true]);
 
 /* Routes for guests */
 Route::group(['middleware' => 'guest'], function () {
