46db59e78356ae9949ebe9b0d541bb307554fe17
Michael Lackenberger <milack68@MacMini.local>
2021-02-19 15:59:20 +0100

    added feature oAuth with github

--- config/services.php
+++ config/services.php
@@ -34,5 +34,10 @@ return [
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
         'redirect' => env('GOOGLE_REDIRECT_URL'),
     ],
+    'github' => [
+        'client_id' => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect' => env('GITHUB_REDIRECT_URL'),
+    ],
 
 ];
