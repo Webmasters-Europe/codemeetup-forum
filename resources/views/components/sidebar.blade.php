72efbf3226846f7e5da6f67d15d824d695f0a8e2
Rhea Marstaller <r.marstaller@gmail.com>
2021-02-27 13:46:34 +0100

    Add logout form to sidebar.

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,13 +1,19 @@
 <div class="col-lg-2 border my-2 py-2">
     @auth
-    <a href="{{ url('/') }}" class="text-sm text-gray-700 underline">Home</a>
-    <a href="{{ route('posts.create') }}">Create Post</a>
-    @else
-    <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
+        <a href="{{ url('/') }}" class="text-sm text-gray-700 underline">Home</a>
+        <a href="{{ route('posts.create') }}">Create Post</a>
 
-    @if (Route::has('register'))
-    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-    @endif
+        <form action="{{ route('logout') }}" method="POST" class="w-50">
+            @csrf
+            <button type="submit" class="btn btn-dark btn-lg ml-2">Logout</button>
+        </form>
+
+    @else
+        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
+
+        @if (Route::has('register'))
+            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+        @endif
     @endauth
-    
+
 </div>
