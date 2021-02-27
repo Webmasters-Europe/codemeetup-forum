e2db56069f0afe34d489974c6b08ed2bb1f0ea29
Rhea Marstaller <r.marstaller@gmail.com>
2021-02-27 13:45:59 +0100

    Add category classes and migration.

--- database/migrations/2021_02_21_105007_create_posts_table.php
+++ database/migrations/2021_02_21_105007_create_posts_table.php
@@ -19,6 +19,8 @@ class CreatePostsTable extends Migration
             $table->text('content');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
             $table->timestamps();
         });
     }
---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Category::create(['name' => 'Katze', 'description' => 'Für Thomas']);
+        Category::factory(9)->create();
+    }
+}
