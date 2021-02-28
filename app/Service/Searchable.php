e6f4b7dcc1b9f06c9b65f7f3caf3997994069654
Cosnavel <coding@niclaskahlmeier.de>
2021-02-28 19:44:45 +0100

    fixed searchable

--- app/Service/Searchable.php
+++ app/Service/Searchable.php
@@ -6,11 +6,20 @@ trait Searchable
 {
     public static function search($search, $columns)
     {
-        $query = self::query();
-        foreach ($columns as $key => $column) {
-            $key == 0 ? $query = $query->where($column, 'like', "%{$search}%") : $query = $query->orWhere($column, 'like', "%{$search}%");
+        if (! isset($search) | ! isset($columns) | empty($search)) {
+            return;
         }
 
-        return $query->get();
+        if (is_array($columns)) {
+            $query = self::query();
+            foreach ($columns as $key => $column) {
+                $key == 0 ? $query = $query->where($column, 'like', "%{$search}%") : $query = $query->orWhere($column, 'like', "%{$search}%");
+            }
+
+            return $query->get();
+        }
+        if (is_string($columns)) {
+            return self::where($columns, 'like', "%{$search}%");
+        }
     }
 }
