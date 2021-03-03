b14173205421cf64f82c4321bf1e28a9f75009f0
milack68 <michael@lackenberger.at>
2021-03-03 11:18:41 +0100

    added Factory and Seeder for User, Post and PostReply

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,9 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+    
         $this->call([
-            CategorySeeder::class
+            UserSeeder::class,
+            CategorySeeder::class,
+            PostSeeder::class,
+            PostReplySeeder::class
         ]);
+
     }
 }
---
+++ database/seeders/PostReplySeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\PostReply;
+use Illuminate\Database\Seeder;
+
+class PostReplySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        PostReply::factory(100)->create();
+    }
+}
