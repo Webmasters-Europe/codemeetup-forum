bce2d3c3e25b62033ca322a9f9a87f60d0111055
Rhea Marstaller <r.marstaller@gmail.com>
2021-03-06 14:51:27 +0100

    Add missing use statement.

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\PostReplyController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
