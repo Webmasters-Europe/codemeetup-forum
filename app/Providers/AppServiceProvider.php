a0b6086cbfafebf53a7b6b94afd3aa5bd616c433
Cosnavel <coding@niclaskahlmeier.de>
2021-04-17 12:26:23 +0200

    added setting singleton to change application settings in runtime

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Setting;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
@@ -24,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->app->singleton(Setting::class, function () {
+            return Setting::first();
+        });
+        config(['app.settings' => app()->make(Setting::class)]);
         Paginator::useBootstrap();
     }
 }
