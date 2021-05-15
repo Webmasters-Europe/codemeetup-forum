7fee4bd92e0ffad6fb444c739e17559aa8a54808
milack68 <milack68@users.noreply.github.com>
2021-05-15 18:10:06 +0000

    Apply prettier changes

--- resources/js/toggleLoginRegistration.js
+++ resources/js/toggleLoginRegistration.js
@@ -1,6 +1,6 @@
 window.toggleLoginRegistration = () => {
-    document.querySelector("#login-form").toggleAttribute("hidden");
-    document.querySelector("#reg-form").toggleAttribute("hidden");
-    document.querySelector("#to-registration").toggleAttribute("hidden");
-    document.querySelector("#to-login").toggleAttribute("hidden");
+    document.querySelector('#login-form').toggleAttribute('hidden')
+    document.querySelector('#reg-form').toggleAttribute('hidden')
+    document.querySelector('#to-registration').toggleAttribute('hidden')
+    document.querySelector('#to-login').toggleAttribute('hidden')
 }
