4bb5503f9ef5f0404007e0742f1a269c659adcc8
Michael Lackenberger <45495538+milack68@users.noreply.github.com>
2021-05-17 20:55:41 +0200

    test for delete and update PostReplies

--- tests/Feature/Http/Controllers/PostReplyControllerTest.php
+++ tests/Feature/Http/Controllers/PostReplyControllerTest.php
@@ -107,4 +107,153 @@ class PostReplyControllerTest extends TestCase
 
         $response->assertRedirect(route('home'));
     }
+
+
+    /**
+     * @test
+     */
+    public function a_moderator_can_delete_post_replies()
+    {
+        $moderator = User::factory()->create()->assignRole('moderator');
+        $user = User::factory()->create()->assignRole('user');
+        $post = Post::factory()->create();
+        $postReply = PostReply::factory()->create();
+        $content = $this->faker->sentence;
+
+        $this->from(route('posts.show', $post->id))
+            ->actingAs($user)
+            ->post(route('replies.store', [$post, $postReply]), [
+            'content' => $content,
+        ]);
+        
+        $this->assertDatabaseHas('post_replies', [
+            'content' => $content,
+        ]);
+        
+        $this->actingAs($moderator);
+        $this->delete(route('replies.destroy', $postReply));
+        $this->assertSoftDeleted('post_replies', [
+            'id' => $postReply->id
+        ]);
+        
+    }
+
+
+    /**
+     * @test
+     */
+    public function a_user_can_delete_his_own_post_replies()
+    {
+        $user = User::factory()->create()->assignRole('user');
+        $post = Post::factory()->create();
+        $postReply = PostReply::factory()->create();
+        $content = $this->faker->sentence;
+
+        $this->from(route('posts.show', $post->id))
+            ->actingAs($user)
+            ->post(route('replies.store', [$post, $postReply]), [
+            'content' => $content,
+        ]);
+        
+        $this->assertDatabaseHas('post_replies', [
+            'content' => $content,
+        ]);
+        
+        $this->actingAs($user);
+        $response = $this->delete(route('replies.destroy', $postReply));
+        $response->assertStatus(302);
+        $this->assertSoftDeleted('post_replies', [
+            'id' => $postReply->id
+        ]);
+
+    }
+
+
+     /**
+     * @test
+     */
+    public function a_user_cannot_delete_post_replies_from_other_users()
+    {
+        $user = User::factory()->create()->assignRole('user');
+        $post = Post::factory()->create();
+        $postReply = PostReply::factory()->create();
+        $content = $this->faker->sentence;
+
+        $this->from(route('posts.show', $post->id))
+            ->actingAs($user)
+            ->post(route('replies.store', [$post, $postReply]), [
+            'content' => $content,
+        ]);
+        
+        $this->assertDatabaseHas('post_replies', [
+            'content' => $content,
+        ]);
+        
+        $this->user2 = User::factory()->create()->assignRole('user');
+
+        $this->actingAs($this->user2);
+        $response = $this->delete(route('replies.destroy', $postReply));
+        $response->assertStatus(403);
+        
+        $this->assertDatabaseHas('post_replies', [
+            'content' => $content,
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function a_moderator_can_update_post_replies()
+    {
+        $moderator = User::factory()->create()->assignRole('moderator');
+        // create reply
+        $post = Post::factory()->create();
+        $user = User::factory()->create();
+        $reply = new PostReply();
+        $reply->content = 'Content';
+        $reply->post()->associate($post);
+        $reply->user()->associate($user);
+        $reply->save();
+
+        $response = $this
+        ->actingAs($moderator)
+        ->patch(route('replies.update', $reply->id), [
+            'content' => 'updated content'
+        ]);
+        $response->assertSessionHasNoErrors();
+
+        $this->assertDatabaseHas('post_replies', [
+            'content' => 'updated content'
+        ]);
+
+        $response->assertRedirect(route('posts.show', $reply->post_id));
+        $response->assertSessionHas('status', 'Reply has been updated.');
+        
+    }
+
+    public function a_user_cannot_update_post_replies()
+    {
+        // create reply
+        $post = Post::factory()->create();
+        $user = User::factory()->create();
+        $reply = new PostReply();
+        $reply->content = 'Content';
+        $reply->post()->associate($post);
+        $reply->user()->associate($user);
+        $reply->save();
+
+        $response = $this
+        ->actingAs($user)
+        ->patch(route('replies.update', $reply->id), [
+            'content' => 'updated content'
+        ]);
+        $response->assertSessionHasNoErrors();
+
+        $this->assertDatabaseMissing('post_replies', [
+            'content' => 'updated content'
+        ]);
+        
+    }
+
+
 }
