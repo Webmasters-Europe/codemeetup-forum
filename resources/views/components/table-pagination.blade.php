4713c7f416327a113642b56e946a571f2ae8a1bd
Alena Vershinina <azudkova@gmail.com>
2021-05-22 13:04:35 +0200

    small fixes

--- resources/views/components/table-pagination.blade.php
+++ resources/views/components/table-pagination.blade.php
@@ -1,9 +1,9 @@
 <div class="d-flex">
 
-    <div class="d-flex align-items-center ml-4">
+    <div class="d-flex align-items-center ml-4 m-2">
         <label for="paginate" class="text-nowrap mr-2 mb-0">{{ __('Per Page') }}</label>
         <select wire:model="paginate" name="paginate" id="paginate"
-                class="form-control form-control-sm">
+                class="form-select form-select-sm">
             <option value="10">10</option>
             <option value="25">25</option>
             <option value="50">50</option>
